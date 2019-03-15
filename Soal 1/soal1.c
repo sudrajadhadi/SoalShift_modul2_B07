6f1c539d392cbf0de8b29764fcedb3819f061b11
Hazimi <36977569+xhazimix@users.noreply.github.com>
2019-03-15 19:16:45 +0700

    updating soal1

--- Soal 1/soal1.c
+++ Soal 1/soal1.c
@@ -1,1 +1,57 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <string.h>
+#include <dirent.h>
 
+int main() {
+  pid_t pid, sid;
+  pid = fork();
+  if (pid < 0) exit(EXIT_FAILURE);
+  if (pid > 0) exit(EXIT_SUCCESS);
+
+  umask(0);
+  sid = setsid();
+  if (sid < 0)  exit(EXIT_FAILURE);
+
+  if ((chdir("/home/xhazimix/Documents/")) < 0) exit(EXIT_FAILURE);
+
+  close(STDIN_FILENO);
+  close(STDOUT_FILENO);
+  close(STDERR_FILENO);
+
+  while(1) {
+   DIR *directory;
+    struct dirent *dir;
+    directory = opendir("/home/xhazimix/Documents/");
+
+    if (directory)
+    {
+        while ((dir = readdir(directory)) != NULL)
+        {
+	    char namafile[200];
+	    int cpy = (int) strlen(dir->d_name);
+	    char *nama = dir->d_name;
+	    if(nama[cpy-4] == '.' && nama[cpy-3]=='p' && nama[cpy-2]=='n' && nama[cpy-1] == 'g'){
+		strcpy(namafile, "/home/xhazimix/Documents/modul2/gambar/");
+		strcat(namafile, nama);
+		cpy = (int)strlen(namafile);
+		namafile[cpy-4] = '\0';
+		strcat(namafile, "_grey.png");
+		if (fork() == 0){
+			char *argv[] = {"mv", nama, namafile, NULL};
+			execv("/bin/mv", argv);
+		}
+            }
+        }
+        closedir(directory);
+    }
+    sleep(5);
+  } 
+  exit(EXIT_SUCCESS);
+}
